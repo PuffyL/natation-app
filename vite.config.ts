b46acc33c2ffcece1f478e2a352c3bb33c7d9653
Leo Germain <leo.germain1@gmail.com>
2025-09-04 15:55:12 +0200

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: '/natation-app/',
+=======
+  base: '/natation-app/'
+>>>>>>> f0bb1d7 (Fix vite base path)
 })
